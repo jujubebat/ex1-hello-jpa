51704f859006aa5a9e608cd32064d1176c5d868b
jujubebat <bbwwpark@naver.com>
2021-06-16 15:06:57 +0900

    feat: 비영속, 영속 학습 완료

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            // 객체를 생성한 상태(비영속)
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            // 객체를 저장한 상태(영속)
+            System.out.println("====BEFORE====");
+            em.persist(member);
+            System.out.println("====AFTER====");
+
+            tx.commit(); // 이 시점에서 실제로 쿼리가 날아감
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+    }
+
+}
