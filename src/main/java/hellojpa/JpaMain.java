0e7130ba9a6aad574dc62dc635b3b385bf804125
jujubebat <bbwwpark@naver.com>
2021-06-16 15:44:02 +0900

    feat: 엔티티 수정(변경 감지 : 더티체킹) 학습 완료 - jpa는 트랜잭션을 커밋하는 시점에 내부적으로 플러시가 호출된다. 그리고 엔티티와 스냅샷 비교를 한다. 1차 캐시안에는 각 엔티티에 해당하는 스냅샷이 있다. 스냅샷은 최초로 영속성 컨텍스트에 들어온 객체의 상태다. 커밋되는 시점에 각 엔티티와 스냅샷을 비교를 한다. 만약 엔티티와 스냅샷이 다르다면 업데이트 쿼리를 만들어서 db에 날린다. (엔티티 삭제하는 경우도 마찬가지!)

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,9 @@ public class JpaMain {
 
         try {
             // 영속
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
-
-            // 영속성 컨텍스트에 member1, member2 객체가 저장된다. 그리고 2개의 insert 문에 대한 쿼리문도 저장된다.
-            em.persist(member1);
-            em.persist(member2);
-            System.out.println("=============");
-
-            // 이떄 영속성 컨텍스트에 있던 2개의 insert 쿼리문을 한 방에 날릴 수 있다. db 접근을 1번만 할 수 있다는 이점이 있다.
+            Member member = em.find(Member.class, 150L);
+            member.setName("Z"); // 이렇게만 해도 업데이트 쿼리가 자동으로 날아간다.
+            System.out.println("===============");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
