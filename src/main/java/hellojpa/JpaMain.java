52b495b9406f9b052b72d4afd3e4a2d46578c520
jujubebat <bbwwpark@naver.com>
2021-06-16 16:15:14 +0900

    feat: 준영속 상태 학습 완료

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,22 +16,22 @@ public class JpaMain {
 
         try {
             // 영속
-            Member member = new Member(201L, "member200");
-            em.persist(member); // member와 쿼리문이 영속성 컨텍스트에 저장된다.
+            Member member = em.find(Member.class, 150L);
+            member.setName("A");
 
-            // 영속성 컨텍스트에 저장된 쿼리문이 DB에 반영된다.
-            // 플러시를 한다고 1차캐시가 지워지는 것은 아님!
-            // 오직 엔티티 변화를 감지하고 영속성 컨텍스트에 있는 쓰지 전용 SQL 저장소에 있는 쿼리를 날린다.
-            em.flush();
+            // 영속성 컨텍스트에서 memeber를 제거한다. member 객체는 준영속 상태가 된다.
+            em.detach(member);
 
-            System.out.println("===========");
-            tx.commit();
+            // 준영속 상태를 만드는 다른 방법 2가지 
+            // em.close(); // 영속성 컨텍스트를 완전히 초기화
+            // em.close(); // 영속성 컨텍스트를 종료
+
+            tx.commit(); // 업데이트가 되지 않음.
         } catch (Exception e) {
             tx.rollback();
         } finally {
             em.close();
         }
-
     }
 
 }
