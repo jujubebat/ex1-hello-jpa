0ae8f316dafc1fd03ce9badf190f694a5989491c
jujubebat <bbwwpark@naver.com>
2021-06-16 15:15:14 +0900

    feat: 영속성 컨텍스트의 1차 캐시에서 조회하기 학습 완료

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,13 +17,19 @@ public class JpaMain {
         try {
             // 객체를 생성한 상태(비영속)
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJPA");
 
             // 객체를 저장한 상태(영속)
             System.out.println("====BEFORE====");
             em.persist(member);
             System.out.println("====AFTER====");
+
+            // 영속성 컨텍스트의 1차 캐시에서 조회하기 때문에 select 쿼리가 따로 안나간다!
+            Member findMember = em.find(Member.class, 101L);
+
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
 
             tx.commit(); // 이 시점에서 실제로 쿼리가 날아감
         } catch (Exception e) {
