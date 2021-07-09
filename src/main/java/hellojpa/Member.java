e81c2efd5cc07f07c1d64a9ec0cba220cfd4d995
jujubebat <bbwwpark@naver.com>
2021-07-09 11:20:49 +0900

    feat: 일대다 양방향 학습 완료 - 이런 매핑은 공식적으로 존재 x - 읽기 전용 필드를 사용해서 양방향처럼 사용하는 방법 - 다대일 양방향을 사용하자

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Member {
@@ -16,6 +18,10 @@ public class Member {
 
     @Column(name = "USERNAME")
     private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    private Team team;
 
     public Long getId() {
         return id;
