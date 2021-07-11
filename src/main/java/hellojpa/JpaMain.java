36c66ba82a451acf303efe8eae1167401d367c67
jujubebat <bbwwpark@naver.com>
2021-07-11 10:08:57 +0900

    feat: 프록시 학습 완료 4 - 영속성 컨텍스트에 찾는 엔티티가 이미 있으면 em.getReference()를 호출해 도 실제 엔티티 반환

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,22 +18,16 @@ public class JpaMain {
             member1.setUsername("member1");
             em.persist(member1);
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            em.persist(member2);
-
             em.flush();
             em.clear();
 
             Member m1 = em.find(Member.class, member1.getId());
-//            Member m2 = em.find(Member.class, member2.getId());
-//            System.out.println("m1 == m2: " + (m1.getClass() == m2.getClass()));
+            System.out.println("m1 = " + m1.getClass());
 
-            // 프록시 객체 비교는 instanceof로 해야한다.
-            Member m2 = em.getReference(Member.class, member2.getId());
-            System.out.println("m1 == m2: " + (m1.getClass() == m2.getClass()));
-            System.out.println("m1 instanceof Member: " + (m1 instanceof Member));
-            System.out.println("m2 instanceof Member: " + (m2 instanceof Member));
+            // reference는 프록시가 아니다. 위에서 이미 영속성 컨텍스트에 Member를 올려뒀기 때문!
+            Member reference = em.getReference(Member.class, member1.getId());
+            System.out.println("reference.getClass() = " + reference.getClass());
+            System.out.println("a == a: " + (m1 == reference));
 
             tx.commit();
         } catch (Exception e) {
