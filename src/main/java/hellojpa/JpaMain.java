3507314cf7ac540d8cd4e666cfbffea715656065
Byung Wook Park <bbwwpark@naver.com>
2021-11-24 13:45:04 +0900

    feat: 네이티브 SQL로 Member 엔티티 조회하기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -5,9 +5,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 public class JpaMain {
 
@@ -19,12 +16,8 @@ public class JpaMain {
 
         try {
 
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Member> query = cb.createQuery(Member.class);
-            Root<Member> m = query.from(Member.class);
-
-            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
-            List<Member> resultList = em.createQuery(cq).getResultList();
+            String sql = "SELECT MEMBER_ID, city, street, zipcode, USERNAME from MEMBER";
+            List<Member> resultList = em.createNativeQuery(sql, Member.class).getResultList();
 
             tx.commit();
         } catch (Exception e) {
